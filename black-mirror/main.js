3cf142041be842a0b8ee4ef499e747a6d0cc7823
Tim <admin@bijzorg.com>
2021-10-11 16:53:07 +0200

    added server listnen

--- black-mirror/main.js
+++ black-mirror/main.js
@@ -1,56 +1,39 @@
-const fs = require('fs')
-const mainconfig = JSON.parse(fs.readFileSync('./mainconfig.json'))
 const app = require('express')();
 const server = require('http').Server(app);
-const bodyParser = require('body-parser');
 const Gpio = require('onoff').Gpio
 
 require('dotenv').config()
 
-server.listen(process.env.BLACK_MIRROR_SWITCH_PORT)
 
-// app.post('/action', function (req, res) {
-//     if (req.body.pin) {
-//         const gebouw = req.body.pin.gebouw
 
-//         if (req.body.action === true) {
+module.exports = {
+    start: function () {
+        //make sure it's off
+        server.listen(process.env.BLACK_MIRROR_SWITCH_PORT)
+
+        app.get('/on', function (req, res) {
             
-//             for (let rel of req.body.pin.relais) {
-//                 pins['pin' + relais[gebouw][rel.relais]].writeSync(1)
-//             }
-//         }
+            const pins = process.env.LIGHT_PINS_ARRAY.split(',')
 
-//         if (req.body.action === false) {
-//             for (let rel of req.body.pin.relais) {
-//                 pins['pin' + relais[gebouw][rel.relais]].writeSync(0)
-//             }
-//         }
+            pins.forEach(pin => {
+                var led = new Gpio(parseInt(pin), 'out');
+                led.writeSync(1)
+            });
+            
+            res.send()
+
+        })
+
+        app.get('/off', function (req, res) {
         
-//     }
+            const pins = process.env.LIGHT_PINS_ARRAY.split(',')
 
-//     res.send()
-// })
+            pins.forEach(pin => {
+                var led = new Gpio(parseInt(pin), 'out');
+                led.writeSync(0)
+            });
 
-app.get('/on', function (req, res) {
-    
-    const pins = process.env.LIGHT_PINS_ARRAY.split(',')
-
-    pins.forEach(pin => {
-        var led = new Gpio(parseInt(pin), 'out');
-        led.writeSync(1)
-    });
-
-    res.send()
-})
-
-app.get('/off', function (req, res) {
-    
-    const pins = process.env.LIGHT_PINS_ARRAY.split(',')
-
-    pins.forEach(pin => {
-        var led = new Gpio(parseInt(pin), 'out');
-        led.writeSync(0)
-    });
-
-    res.send()
-})
+            res.send()
+        })
+    },
+};
--- main.js
+++ main.js
@@ -1,14 +1,15 @@
 // var test = require('./test');
 var lightsensor = require('./lightSensor/main.js')
+var blackmirror = require('./black-mirror/main.js')
 require('dotenv').config()
 
 
 if (process.env.BLACK_MIRROR_SWITCH === "true") {
-    console.log('test')
+    blackmirror.start()
 }
 
 if (process.env.LIGHT_TOGGLE_SENSOR === "true") {
-    lightsensor.start()
+    // lightsensor.start()
 }
 
 if (process.env.TEMP_SENSOR === "true") {
